711c64e1ab65f73f9f9c39f4d978ccdaff49df1b
Tsuna2710 <jhuarancac@unsa.edu.pe>
2023-10-02 17:28:06 -0500

    falta fecha de naci

--- Practica01/Student.java
+++ Practica01/Student.java
@@ -2,15 +2,37 @@ public class Student {
     String Name;
 	String Materno;
 	String Paterno;
-    public Student(String Paterno, String Materno, String Name){
+    String Gender;
+    String CUI;
+    String Status;
+    String Email;
+
+
+    public Student(String Paterno, String Materno, String Name, String Gender,String CUI,String Status){
         this.Materno=Materno;
         this.Name=Name;
         this.Paterno=Paterno;
+        this.Gender=Gender;
+        this.CUI=CUI;
+        this.Status=Status;
+        this.Email=generarEmail();
     }
+
     public String getName(){
 		return Name;
 	}
+    
+    public String getStatuS(){
+		return Status;
+	}
 
+    public String getGender(){
+		return Gender;
+	}
+
+    public String getCUI(){
+		return CUI;
+	}
 	public String getMaterno(){
 		return Materno;
 	}
@@ -19,8 +41,12 @@ public class Student {
 		return Paterno;
 	}
 
+    public String generarEmail(){
+		return Name.charAt(0)+Paterno+Name.charAt(0)+"@unsa.edu.pe";
+	}
+
     public String toString(){
-        return Paterno+"\t"+Materno+"\t"+Name;
+        return Paterno+"\t"+Materno+"\t"+Name+"\t"+Gender+"\t"+CUI+"\t"+Status+"\t"+Email.toLowerCase();
     }
     
 }
--- Practica01/main.java
+++ Practica01/main.java
@@ -1,20 +1,21 @@
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class main {
     public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
         ArrayList<Student> Lista = new ArrayList<>();
-        String Nombre,Materno,Paterno;
         Student Estudiante;
-
+        
         try{
             BufferedReader reader= new BufferedReader(new FileReader("Datos.csv"));
             String line = null;
             while((line=reader.readLine()) != null){
                 String[] parts=line.split(";");
-                Estudiante=new Student(parts[0],parts[1],parts[2]);
+                Estudiante=new Student(parts[0],parts[1],parts[2],parts[3],parts[4],parts[5]);
                 Lista.add(Estudiante);
             }
         }catch(IOException e){
@@ -24,15 +25,24 @@ public class main {
         System.out.println(Lista.get(i).toString());
         }
         
-        ordenarPorNombreBurbuja(Lista);
+        //ordenarPorNombreBurbuja(Lista);
         System.out.println("==========================");
         System.out.println("==========================");
         System.out.println("==========================");
-        System.out.println("Ordenar por nombre");
+        //System.out.println("Ordenar por nombre");
 
-        for(int i=0;i<Lista.size(); i++){
-        System.out.println(Lista.get(i).getName());
-        }
+        //for(int i=0;i<Lista.size(); i++)
+          //  System.out.println(Lista.get(i).getName());
+
+        System.out.println("Ingrese el nombre de a nave a buscar: ");
+		String nombre=sc.next();
+        int pos=busquedaBinariaNombre(Lista,nombre);
+        
+        if (pos!=-1) {
+			System.out.println(Lista.get(pos).toString());
+		}
+		else System.out.println("No encontrado");
+
     }
     public static void ordenarPorNombreBurbuja ( ArrayList<Student> list ){
         for(int i=0;i< list.size() ;i++){
@@ -43,12 +53,18 @@ public class main {
             }
         }
     }
-    
-        public static void intercambiar ( ArrayList<Student> list,int i, int j){
-            Student aux ;
-            aux= list.get(i);
-            list.set(i, list.get(j));
-            list.set(j, aux);
-            }
+    public static void intercambiar ( ArrayList<Student> list,int i, int j){
+        Student aux ;
+        aux= list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, aux);
+    }
+
+    public static int busquedaBinariaNombre(ArrayList<Student> list, String s){
+		for(int i=0;i<list.size();i++){
+			if(s.equals(list.get(i).getName())) return i;
+		}
+		return -1;   
+	}
 
 }
